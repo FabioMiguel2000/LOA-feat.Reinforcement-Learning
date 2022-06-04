144e601830d2d5b0a17486ca9f78fdf83f2f0f16
Fab <fabiomiguelchenhuang@gmail.com>
2022-06-04 17:46:38 +0100

    Added trained model

--- src/main.py
+++ src/main.py
@@ -2,33 +2,51 @@
 from pickle import FALSE
 from game.constants import *
 from envs.loa_env import LoaEnv
-from stable_baselines3 import A2C
+from stable_baselines3 import A2C, PPO
 from stable_baselines3.common.env_checker import check_env
 
 import pygame
 import numpy as np
 import gym
 import time
+import os
+
+
 
 WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
 FPS = 60
-EPISODES_NUM = 1000
+EPISODES_NUM = 100
 ACTION_SPACE = (BOARD_SIZE -2) * 2 * 4
+TIMESTEPS = 10000
 
-def main():
-    
-    clock = pygame.time.Clock()     # Object to help track time
+models_dir = f"models/"
+log_dir = f"logs/PPO-{int(time.time())}"
+
+if not os.path.exists(models_dir):
+    os.makedirs(models_dir)
+
+# if not os.path.exists(log_dir):
+#     os.makedirs(log_dir)
+
+
+def main(MODEL):
+    clock = pygame.time.Clock()     # Object to help track timeƒ
     env = LoaEnv(WINDOW)
     STOP = False            # Game Loop Flag
          
-    model = A2C("MlpPolicy", env, verbose=1)
-    model.learn(total_timesteps=100)
-
+    if(MODEL == "PPO"):
+        model = PPO.load(f"{models_dir}/{MODEL}_{TIMESTEPS}_STEPS_MODEL", env=env)
+    elif MODEL == "A2C":
+        model = A2C.load(f"{models_dir}/{MODEL}_{TIMESTEPS}_STEPS_MODEL", env=env)
+    
     all_episode_rewards = []
     for episode in range (1, EPISODES_NUM + 1):
-        env.reset()
+        observation = env.reset()
+        env.render()
+        print(env.game.board.npBoard)
         episode_rewards = []
         done = False
+        # time.sleep(1)
         while not done:
 
             # ----------- GUI stuff -------------
@@ -39,25 +57,19 @@ def main():
                     done = True    
                     break
                 
-            env.render()
-            print(env.game.board.npBoard)
-            
             # -----------------------------------
             
-
-            # action = np.random.randint(ACTION_SPACE)    # Random Action
-            random_action = env.action_space.sample()
-            # print(action)
-            observation, reward, done, info = env.step(random_action)
-            # if(reward != 0):
-            #     time.sleep(1)
+            # action = env.action_space.sample()   # Random Action
+            action, _ = model.predict(observation, deterministic=True)
+            observation, reward, done, info = env.step(action)
             episode_rewards.append(reward)
-            print(reward)
-            
 
-            if(done):
+            if(reward != 0):
                 env.render()
-                print(env.game.board.npBoard)
+                print_status(action, env.game.board.npBoard, reward)
+                # time.sleep(1)
+            
+            if(done):
                 env.close()
                 print("Episode = ", episode, "has ended!\n")
         
@@ -69,12 +81,41 @@ def main():
     print("Mean reward:", mean_episode_reward, "Num episodes:", EPISODES_NUM)
     pygame.quit()
 
+def print_move(action):
+    pieceN = (action // 4) + 1
+    moveDir = action % 4
+    if moveDir == 0:
+        print("Piece " +str(pieceN)+" moved UP")
+    elif moveDir == 1:
+        print("Piece " +str(pieceN)+" moved RIGHT")
+    elif moveDir == 2:
+        print("Piece " +str(pieceN)+" moved DOWN")
+    elif moveDir == 3:
+        print("Piece " +str(pieceN)+" moved LEFT")
+    else:
+        print("WRONG DIRECTION")
+
+def print_status(action, board, reward):
+    print(board)
+    print_move(action)
+    print("Reward = " + str(reward))
 
 
-main()
+def train_model(MODEL):
+    env = LoaEnv(WINDOW)
+    if(MODEL == "PPO"):
+        model = PPO("MlpPolicy", env, verbose=1)
+    elif MODEL == "A2C":
+        model = A2C("MlpPolicy", env, verbose=1)
+    
+    model.learn(total_timesteps=TIMESTEPS, reset_num_timesteps= False, tb_log_name=model)
+    filename = f"{models_dir}/{MODEL}_{TIMESTEPS}_STEPS_MODEL"
+    model.save(filename)
+    return filename
 
-# check_env
-# env = LoaEnv(WINDOW)
-# check_env(env)
+model = "PPO"
+# train_model(model)    #to train a model
+main(model)
+
 
 
